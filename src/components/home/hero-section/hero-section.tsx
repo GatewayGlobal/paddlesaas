a82e4c0b0e65e254062acca6395ed6127e83d32a
GatewayGlobal <67723485+GatewayGlobal@users.noreply.github.com>
2024-11-04 13:14:02 +0000

    hero revised

--- src/components/home/hero-section/hero-section.tsx
+++ src/components/home/hero-section/hero-section.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 export function HeroSection() {
   return (
     <section className={'mx-auto max-w-7xl px-[32px] relative flex items-center justify-between mt-16 mb-12'}>
       <div className={'text-center w-full '}>
         <h1 className={'text-[48px] leading-[48px] md:text-[80px] md:leading-[80px] tracking-[-1.6px] font-medium'}>
-          Powerful design toolss.
+          Powerful design tools.
           <br />
           Simple pricing.
         </h1>
